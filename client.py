f1bb339d7737dda773f74b5045d2ce9fef185a6b
TanguyLichau <tanguylichau@gmail.com>
2022-12-31 11:45:25 +0100

    Added client file

--- client.py
+++ client.py
@@ -1,1 +1,11 @@
-print('test')
+import socket
+
+# initializing the socket
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+IP = "192.168.1.54"  # the ip of the server.
+PORT = 5050  # the port of the server.
+
+soc.connect((IP, PORT))  # connecting to the server.
+
+print(soc.recv(1024).decode('ascii'))
